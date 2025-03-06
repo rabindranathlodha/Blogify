e495ae36c3d99c67cd171d6b11c7868be3b6b1a2
Rabindranath Lodha <rabilodha789@gmail.com>
2025-03-06 19:50:48 +0530

    tokens created

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { createHmac, randomBytes } = require('crypto');
 const { Schema, model } = require('mongoose');
+const { createTokenForUser } = require('../services/authentication');
 
 const userSchema = new Schema({
     fullName: {
@@ -44,7 +45,7 @@ userSchema.pre('save', function (next) {
     next();
 });
 
-userSchema.static('matchPassword', async function (email, password) {
+userSchema.static('matchPasswordAndGenerateToken', async function (email, password) {
     const user = await this.findOne({ email });
     if(!user) throw new Error('User not found');
     
@@ -57,8 +58,8 @@ userSchema.static('matchPassword', async function (email, password) {
 
     if(userProvidedHash !== hashedPaasword) throw new Error('Password not matched');
 
-    return user;
-
+    const token = createTokenForUser(user);
+    return token;
 });
 
 
