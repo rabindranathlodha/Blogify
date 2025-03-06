6fd467616eb7a5624d3c21f4ff4a7871f2b57596
Rabindranath Lodha <rabilodha789@gmail.com>
2025-03-06 23:48:16 +0530

    blog uploading sucess

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieparser = require('cookie-parser');
 
+const Blog = require('./models/blog');
+
 const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
+
+
 const { checkForAuthCookie } = require('./middlewares/authentication');
 
 
@@ -13,6 +18,7 @@ const PORT = 8000;
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieparser());
 app.use(checkForAuthCookie('token'));
+app.use(express.static(path.resolve('./public')));
 
 mongoose.connect('mongodb://127.0.0.1:27017/blogify')
     .then((e) => console.log('Connected to MongoDB'))
@@ -22,12 +28,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    const allBlogs = await Blog.find({});
     res.render("home",{
         user: req.user,
+        blogs: allBlogs,
     });
 });
 
 app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
